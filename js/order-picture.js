ef90e5cfc71afd38c9182e1670ae8102fb99e78d
evox1994 <dmitrybatukhtin@gmail.com>
2019-04-23 13:38:02 +0700

    sdfvsdfv

---
+++ js/order-picture.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	function containerSize(){
+		var max_width = 0;
+		$('.b-3-twenty-container img').each(function(){
+			if ( $(this).width() > max_width ){
+				max_width = $(this).width();
+			}
+		});
+		$('.b-3-twenty-container').css('max-width',max_width);
+	}
+	containerSize();
+
+	$(window).resize(function(){
+		containerSize();
+	});
+
+	$('.b-3-twenty-container').twentytwenty({
+		before_label: 'Эскиз',
+		after_label: 'Картина'
+	});
+
+});
